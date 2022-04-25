583ecdb08b486b7054929355155960fc63fd0b1c
Maria Eduarda <70414614+Maria-Eduarda90@users.noreply.github.com>
2022-04-25 09:24:54 -0400

    Update portscan.py

--- portscan/portscan.py
+++ portscan/portscan.py
@@ -4,6 +4,6 @@ ports = [21,22,80,443,445,3306,25]
 
 for port in ports:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    code = client.connect_ex(("bancocn.com", port))
+    code = client.connect_ex(("teste.com", port))
     if code == 0:
         print(port, "OPEN")
