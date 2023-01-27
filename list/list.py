0804e9c9e9ae9d6d3d2f3f482d7a452e0fef6afa
Mary Skywalker <snowdennaruto90@gmail.com>
2023-01-26 20:12:07 -0400

    list study

---
+++ list/list.py
@@ -0,0 +1,20 @@
+equipments=[]
+values=[]
+serials=[]
+department=[]
+
+resp = "S"
+
+while resp =="S":
+    equipments.append(input("Equipamento: "))
+    values.append(float(input("Valor: ")))
+    serials.append(int(input("Número serial: ")))
+    department.append(input("Departamento: "))
+    resp = input("Digite \"s\" para continuar: ").upper()
+
+for indice in range(0, len(equipments)):
+    print("\nEquipamento..: ", (indice+1))
+    print("Nome..: ", (equipments[indice]))
+    print("Valor........: ", (values[indice]))
+    print("serial.......: ", (serials[indice]))
+    print("Departamento.: ", (department[indice]))
