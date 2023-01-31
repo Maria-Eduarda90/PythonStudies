72482025dcd75f7cc4dc01274f7efb487aa5aed2
Mary Skywalker <snowdennaruto90@gmail.com>
2023-01-31 17:15:57 -0400

    os

---
+++ os/index.py
@@ -0,0 +1,8 @@
+import platform
+
+print("Nome maquina:...................", platform.node())
+print("Arquitetura:....................", platform.architecture())
+print("Sistema Operacional:............", platform.system())
+print("Versão do SO:...................", platform.release())
+print("Processador:....................", platform.processor())
+print("Versão do python:...............", platform.python_version())
