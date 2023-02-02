46a84ba1e26eeebd14e47ce5c4fa308e9be5046d
Mary Skywalker <snowdennaruto90@gmail.com>
2023-02-02 19:05:19 -0400

    ftp

---
+++ ftp/index.py
@@ -0,0 +1,19 @@
+from ftplib import *
+
+ftp = FTP('ftp.ibiblio.org')
+print(ftp.getwelcome())
+
+user = input("Digite o usuario: ")
+password = input("Digite a senha: ")
+
+ftp.login(userm password)
+
+print("Diretório atual de trabalho: ", ftp.pwd())
+
+ftp.cwd('pub')
+
+print("Diretório corrente: ", ftp.pwd())
+
+print(ftp.retrlines('LIST'))
+
+ftp.quit()
