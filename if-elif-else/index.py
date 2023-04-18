982597db150c47ec854d68919c10683d634f36f4
Maria Eduarda <70414614+Maria-Eduarda90@users.noreply.github.com>
2023-04-17 22:15:49 -0400

    Update index.py

--- if-elif-else/index.py
+++ if-elif-else/index.py
@@ -5,8 +5,8 @@ numero2 = int(input("digite a nota 2: "))
 media = (numero1 + numero2) / 2
 
 if media >= 6:
-    print(f'{nome}, voçê foi aprovado(a)!')
+    print(f'{nome}, você foi aprovado(a)!')
 elif media < 6 and media >= 4:
-    print(f'{nome} voçê ficou de recuperação!')
+    print(f'{nome} você ficou de recuperação!')
 else:
-    print(f'{nome} voçê reprovou :(')
+    print(f'{nome} você reprovou :(')
