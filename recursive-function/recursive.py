43f22553774033fd7508319b25a9a7079fa36e4d
Maria Eduarda <70414614+Maria-Eduarda90@users.noreply.github.com>
2023-05-04 21:26:35 -0400

    Update recursive.py

--- recursive-function/recursive.py
+++ recursive-function/recursive.py
@@ -1,7 +1,7 @@
 def count_numbers(n):
     if n == 0:
         return 1
-    elif n % 2 == 0:
+    elif n%2 == 0:
         return 1 + count_numbers(n-1)
     else:
         return count_numbers(n-1)
