eb6e82950caf23c47a2951cbb40480a88360bb5d
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-02-10 16:53:55 -0300

    dns

---
+++ DNS/dns.py
@@ -0,0 +1,20 @@
+import socket
+import os
+
+diretorio = os.getcwd()
+dominio = input("Alvo: ")
+txt = diretorio + "\\DNS\\bruteforce.txt"
+
+try:
+    with open(txt, "r") as arquivo:
+        bruteforce = arquivo.readlines()
+    
+    for name in bruteforce:
+        DNS = name.strip("\n") + "." + dominio
+        try:
+            print(DNS + ": " + socket.gethostbyname(DNS))
+        except socket.gaierror:
+            pass
+
+except FileNotFoundError:
+    print("Não encontrado")
