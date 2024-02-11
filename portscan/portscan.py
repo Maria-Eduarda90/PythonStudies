4c67b84a9b5a6ad2986858fdaa5627776dcc8ef5
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-02-10 23:10:17 -0300

    port scan

--- portscan/portscan.py
+++ portscan/portscan.py
@@ -1,9 +1,10 @@
 import socket
 
+host = input("Alvo: ")
 ports = [21,22,80,443,445,3306,25]
 
 for port in ports:
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    code = client.connect_ex(("teste.com", port))
+    code = client.connect_ex((host, port))
     if code == 0:
         print(port, "OPEN")
