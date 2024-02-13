803a187f8f6be6788ac6823bc8bc7c9c12be3fc1
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-02-13 00:23:40 -0300

    portscan com o python-nmap

---
+++ scan-nmap/index.py
@@ -0,0 +1,18 @@
+import nmap
+
+nmap_scan = nmap.PortScanner()
+
+nmap_scan.scan('8.8.8.8', '21-80')
+
+for host in nmap_scan.all_hosts():
+    print('Host : %s (%s)' % (host, nmap_scan[host].hostname()))
+    print('State : %s' % nmap_scan[host].state())
+
+    for proto in nmap_scan[host].all_protocols():
+        print('-------------')
+        print('Protocol : %s' % proto)
+        
+        lport = nmap_scan[host][proto].keys()
+
+        for port in lport:
+            print('port: %s\tstate : %s' % (port, nmap_scan[host][proto][port]['state']))
